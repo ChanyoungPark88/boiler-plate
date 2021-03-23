835a33ed879132acee196faab640535a9117fca1
ChanyoungPark88 <chanyoung.p.1988@gmail.com>
2021-03-22 23:52:08 -0600

    clear lecture 12

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const config = require('./config/key')
 const {User} = require('./models/User')
 
 // bodyPaser 설정
 app.use(bodyParser.urlencoded({extended:true})) // application/x-www-form-urlencoded
 app.use(bodyParser.json())                      // application/json
+app.use(cookieParser())
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI,{
@@ -16,9 +18,9 @@ mongoose.connect(config.mongoURI,{
   .catch(err=>console.log(err))
 
 
-app.get('/', (req,res) => res.send('Fuck you world! You guys all sucks!'))
+app.get('/',(req,res)=>res.send('Fuck you world! You guys all suck!'))
 
-app.post('/api/users/register', (req,res) => {
+app.post('/api/users/register',(req,res)=>{
     // 회원가입할 때 필요한 정보들을 클라이언트에서 가져오면 DB에 넣어준다.
     const user = new User(req.body)
     user.save((err,userInfo)=>{
@@ -26,4 +28,35 @@ app.post('/api/users/register', (req,res) => {
         return res.status(200).json({success:true})
     })
 })
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.post('/api/users/login',(req,res)=>{
+    // 요청된 이메일이 DB에 있는지 확인
+    User.findOne({email:req.body.email},(err,userInfo)=>{
+        if(!userInfo){
+            return res.json({
+                loginSuccess:false,
+                message:"제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+        // 요청된 이메일에 대한 비밀번호가 일치하는지 확인
+        userInfo.comparePassword(req.body.password,(err,isMatch)=>{
+            if(!isMatch){
+                return res.json({
+                    loginSuccess:false,
+                    message:"비밀번호가 틀렸습니다"
+                })
+            }else{
+                // 모든 정보가 일치한다면 토큰을 생성
+                userInfo.generateToken((err,user)=>{
+                    if(err) return res.status(400).send(err)
+                    res.cookie('x_auth',userInfo.token)
+                    .status(200)
+                    .json({
+                        loginSuccess:true,userId:userInfo._id
+                    })
+                })
+            }
+        })
+    })
+})
+app.listen(port,()=>console.log(`Example app listening on port ${port}!`))
