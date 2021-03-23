9f03eb07a63a7234ec7d88ec49ea4fb347ea1260
ChanyoungPark88 <chanyoung.p.1988@gmail.com>
2021-03-22 21:59:59 -0600

    error fix

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 const saltRounds = 10
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken')
 
 // 유저 스키마 생성
 const userSchema = mongoose.Schema({
@@ -36,8 +36,9 @@ const userSchema = mongoose.Schema({
 })
 // 유저 스키마 끝
 
+// 유저 모델에 정보를 저장하기 전에 --> pre save
 userSchema.pre('save', function (next) {
-    var user = this;
+    var user = this
     if (user.isModified('password')) {
         //비밀번호를 암호화 시킨다.
         bcrypt.genSalt(saltRounds, function (err, salt) {
@@ -59,13 +60,13 @@ userSchema.methods.comparePassword = function (plainPassword, cb) {
 
     //plainPassword 1234567    암호회된 비밀번호 $2b$10$l492vQ0M4s9YUBfwYkkaZOgWHExahjWC
     bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
-        if (err) return cb(err);
-        cb(null, isMatch);
+        if (err) return cb(err)
+        cb(null, isMatch)
     })
 }
 
 userSchema.methods.generateToken = function (cb) {
-    var user = this;
+    var user = this
     // console.log('user._id', user._id)
 
     // jsonwebtoken을 이용해서 token을 생성하기 
@@ -82,14 +83,14 @@ userSchema.methods.generateToken = function (cb) {
 }
 
 userSchema.statics.findByToken = function(token, cb) {
-    var user = this;
+    var user = this
     // user._id + ''  = token
     //토큰을 decode 한다. 
     jwt.verify(token, 'secretToken', function (err, decoded) {
         //유저 아이디를 이용해서 유저를 찾은 다음에 
         //클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
         user.findOne({ "_id": decoded, "token": token }, function (err, user) {
-            if (err) return cb(err);
+            if (err) return cb(err)
             cb(null, user)
         })
     })
