656f1913c37aca51703e49cb38f1092f75c2cace
ChanyoungPark88 <chanyoung.p.1988@gmail.com>
2021-03-24 01:10:21 -0600

    login lecture

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -2,7 +2,7 @@ import React,{useState} from 'react'
 import {useDispatch} from 'react-redux'
 import {loginUser} from '../../../_actions/user_action'
 
-function LoginPage() {
+function LoginPage(props) {
     const dispatch = useDispatch()
     
     const [Email, setEmail] = useState('')
@@ -25,6 +25,13 @@ function LoginPage() {
         }
 
         dispatch(loginUser(body))
+        .then(response=>{
+            if(response.payload.loginSuccess){
+                props.history.push('/')
+            }else{
+                alert('Error')
+            }
+        })
         
     }
 
